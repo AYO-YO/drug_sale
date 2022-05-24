c03f9f15a4b2aa8582c2f0c6688fc7bc75593e9d
ayo_yo <gxlandzcx@gmail.com>
2022-05-24 21:12:38 +0800

    编码过滤器

---
+++ src/main/java/cn/fanbaby/drug_sale/filter/EncodeFilter.java
@@ -0,0 +1,28 @@
+package cn.fanbaby.drug_sale.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 编码过滤器
+ */
+@WebFilter(filterName = "EncodeFilter")
+public class EncodeFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        resp.setContentType("text/html;charset=utf-8");
+        req.setCharacterEncoding("utf-8");
+        chain.doFilter(req, resp);
+    }
+}
